9055c8c406ccf68aa12203b27008bd2bd0f59a13
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-09-11 08:53:29 -0700

    fix: test command

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
-import { defineProject } from "vitest/dist/config";
+import { configDefaults, defineProject } from "vitest/config";
 
 export default defineProject({
   test: {
     singleThread: true,
     globals: true,
     setupFiles: [".vitest/setupTests.ts"],
+    exclude: [...configDefaults.exclude, "../**/*.test.ts", "../**/*.spec.ts"],
     name: "e2e-tests",
   },
 });
