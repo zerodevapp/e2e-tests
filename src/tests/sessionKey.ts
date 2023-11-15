649790bf1876f704f57ecd5f88b7e40a3456a628
Sahil Vasava <sahilvasava@gmail.com>
2023-11-16 00:55:27 +0530

    feat: update session key with batching

--- src/tests/sessionKey.ts
+++ src/tests/sessionKey.ts
@@ -1,10 +1,10 @@
 import {
   ECDSAProvider,
-  Operation,
-  ParamCondition,
+  ParamOperator,
   SessionKeyProvider,
+  getPermissionFromABI,
 } from "@zerodev/sdk";
-import { getFunctionSelector, pad, type PublicClient } from "viem";
+import { type PublicClient } from "viem";
 import { PrivateKeySigner, type SmartAccountSigner } from "@alchemy/aa-core";
 import type { Contract, Project, Provider } from "../../src/types";
 import { generatePrivateKey } from "viem/accounts";
@@ -61,19 +61,17 @@ export async function sessionKey(
       validAfter: 0,
       validUntil: 9999999999,
       permissions: [
-        {
-          target: erc721.address,
-          valueLimit: 0n,
-          sig: getFunctionSelector("mint(address)"),
-          operation: Operation.Call,
-          rules: [
-            {
-              condition: ParamCondition.EQUAL,
-              offset: 0,
-              param: pad(await ecdsaProvider.getAddress(), { size: 32 }),
-            },
-          ],
-        },
+        getPermissionFromABI({
+            target: erc721.address,
+            abi: erc721.abi,
+            functionName: "mint",
+            args: [
+              {
+                operator: ParamOperator.EQUAL,
+                value: await ecdsaProvider.getAddress(),
+              }
+            ],
+          }),
       ],
     },
   });
